b15ae3c59ae5fef3490d6ea866e8913d5e4b58ee
VictoriaLogan <victoria.logan@heig-vd.ch>
2021-12-09 00:08:45 +0100

    docker fonctionne pas, tous les fichiers sont là

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Build the Docker image locally
-docker build --tag Victoria/API_2021_SMTP-docker .
+docker build --tag bossderu/API_2021_SMTP-docker .
--- docker/run-container.sh
+++ docker/run-container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -p 8282:8282 -p 2525:2525 Victoria/API_2021_SMTP-docker
+docker run -p 8282:8282 -p 2525:2525 bossderu/API_2021_SMTP-docker
