b1648908a453298d26783cc20d4baa0a1e3ea5d7
VictoriaLogan <victoria.logan@heig-vd.ch>
2021-12-08 22:26:40 +0100

    faire fonctionner docker bail

---
+++ docker/build-image.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Build the Docker image locally
+docker build --tag Victoria/API_2021_SMTP-docker .
---
+++ docker/run-container.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -p 8281:8282 -p 8282:8282 VictoriaLogan/SMTP-docker
